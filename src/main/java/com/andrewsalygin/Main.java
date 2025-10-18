95fc0f110113f27d10baa52b7e1e3fad059926ae
Andrew Salygin <andrew.salygin@gmail.com>
2025-10-18 19:02:30 +0400

    Delete register bean

--- src/main/java/com/andrewsalygin/Main.java
+++ src/main/java/com/andrewsalygin/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        context.register(VkWallParserServiceToMongoImpl.class);
 
         VkWallParserServiceToMongoImpl parser = context.getBean(VkWallParserServiceToMongoImpl.class);
         parser.parseAllWalls("all");
